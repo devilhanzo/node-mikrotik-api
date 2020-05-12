4d821f03186acc5c75034d203c76602ca74a26ab
PuckSolo <puck@Skulchai-MBP-3.local>
2020-05-12 16:59:46 +0700

    Update config variables

--- config.js
+++ config.js
@@ -5,11 +5,5 @@ module.exports = {
       user: process.env.ROUTER_USERNAME,
       password: process.env.ROUTER_PASSWORD,
     },
-    router2: {
-      host: process.env.ROUTER2_HOST,
-      port: process.env.ROUTER2_PORT,
-      user: process.env.ROUTER2_USERNAME,
-      password: process.env.ROUTER2_PASSWORD,
-    },
     secret: process.env.SECRET,
   };
